dd826f6e2e828fa11b8fbe8f857959ff9a47d4c2
Tim Smith <tsontario@gmail.com>
2016-03-21 10:01:42 -0400

    Add readme, studentinfo class

---
+++ src/StudentInfo.java
@@ -0,0 +1,34 @@
+
+/**
+ * Contains a method <code>void display()</code> that all the <code>main</code> methods
+ * call to display the student information. Fill the box with your personal
+ * information.
+ * 
+ * @author Marcel Turcotte (turcotte@eecs.uottawa.ca)
+ */
+
+public class StudentInfo {
+
+    /**
+     * Displays the student information: student name, id, section, etc for each
+     * member of the team.
+     */
+
+    public static void display() {
+
+	System.out.println("************************************************************");
+	System.out.println("*                    Timothy Smith                         *");
+	System.out.println("*                       6693759                            *");
+	System.out.println("*                                                          *");
+	System.out.println("*                      Mark Mroz                           *");
+	System.out.println("*                       6846979                            *");
+	System.out.println("*                                                          *");
+	System.out.println("*                    Section --- A                         *");
+	System.out.println("*                      ITI  1121                           *");
+	System.out.println("*                   Assignment   3                         *");
+	System.out.println("************************************************************");
+	System.out.println();
+
+    }
+
+}
---
+++ src/readme.txt
@@ -0,0 +1,8 @@
+ITI 1121 Assignment 3
+Authors:
+Timothy Smith (6693759)
+Mark Mroz (6846979)
+
+This program implements the Circle the Dot game with the added functionality of being able to undo/redo moves, as
+well as being able to save the state of the game to a file that will reopen automatically (if present) upon starting
+the game again.
